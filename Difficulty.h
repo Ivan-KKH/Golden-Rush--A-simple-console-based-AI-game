dcdbda0a4156ba2f8e93b2e9509b569df510574d
Kieran Lo <lokieran@gmail.com>
2020-04-24 22:02:37 +0800

    initialize files

---
+++ Difficulty.h
@@ -0,0 +1,24 @@
+#ifndef DIFFICULTY_H
+#define DIFFICULTY_H
+
+#include <iostream>
+
+#endif
+
+using namespace std;
+
+void Choose_difficulty(string input){
+  if (input == "easy"){
+    cout << "easy" << endl;
+  }
+  if (input == "hard"){
+    cout << "hard" << endl;
+  }
+}
+
+// #ifndef DIFFICULTY_H
+// #define DIFFICULTY_H
+//
+// void Choose_difficulty(string input);
+//
+// #endif
---
+++ Username.h
@@ -0,0 +1,17 @@
+#ifndef USERNAME_H
+#define USERNAME_H
+
+#include <iostream>
+#include <string>
+
+#endif
+
+using namespace std;
+
+string Display_Username(string &username){
+  return username;
+}
+
+void Username_Storage(string &username){
+  // use dynamic array to store username for ranking
+}
