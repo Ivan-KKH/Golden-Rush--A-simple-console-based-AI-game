44e82b6afcec944094553c7a0a80cb1a80f5f7d0
Kieran Lo <lokieran@gmail.com>
2020-05-02 17:06:39 +0800

    initialize score system file

--- Score_system.h
+++ Score_system.h
@@ -8,5 +8,22 @@
 using namespace std;
 
 void score_system(){
+  for (int i=0; i < number_of_resources; ++i){
+    if (player.x == resource[i].x && player.y == resource[i].y){
 
+      if (resource[i] == "diamond"){
+          p1.score += 100;
+      }
+      else if (resource[i] == "gold"){
+          p1.score += 50;
+      }
+      else if (resource[i] == "silver"){
+          p1.score += 20;
+      }
+      else if (resource[i] == "bronze"){
+          p1.score += 10;
+      }
+
+    }    
+  }
 }
