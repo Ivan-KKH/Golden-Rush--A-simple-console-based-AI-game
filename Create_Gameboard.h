51f4a62d9db094634eba212f0973fa040f2e5661
Ivan-KKH <56263623+Ivan-KKH@users.noreply.github.com>
2020-05-02 17:57:43 +0800

    Randomization of player

--- Create_Gameboard.h
+++ Create_Gameboard.h
@@ -19,7 +19,3 @@ void Create_Gameboard(char** board, int board_size){ //to create a gameboard of 
     }
   }
 }
-
-void Random_Resource_Location() {
-  
-}
--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 
 using namespace std;
 
-void Display_Gamerule() {                  //to display the gamerule
+void Display_Gamerule() {          //to display the gamerule
   string s;
   ifstream fin;
   fin.open("gamerule.txt");
@@ -19,7 +19,7 @@ void Display_Gamerule() {                  //to display the gamerule
   while (getline(fin,s))
     cout << s << endl;
   fin.close();
-}                                           //end
+}                                                                       //end
 
 int main() {
   int board_size = 0,difficulty;
@@ -30,35 +30,72 @@ int main() {
     cin >> board_size;
     if (board_size < 5||board_size > 25)
       cout << "The range for the board size is 5x5 to 25x25, please enter the size again" << endl;
-  }                                           //end
+  }                                                                //end
 
   char** board = new char*[board_size+2];    //to create the board array
   for (int i = 0;i < board_size + 2;i++)
-    board[i] = new char[board_size+2];    // end
+    board[i] = new char[board_size+2];
 
   srand(time(NULL));       //to randomize the number of resources on the board
   int number_of_resources = (0.2*board_size * board_size + 1);
-  number_of_resources = rand() % number_of_resources + board_size * board_size * 0.2;                                                      //End
+  number_of_resources = rand() % number_of_resources + board_size * board_size * 0.2;
 
   struct coord {
-    int x,y,type;
+    int x,y;
+    char type;
   };
-  coord resource[number_of_resources];
-  resource[0].x = rand() % (board_size + 1); //to allocate the resources onto the map
-  resource[0].y = rand() % (board_size + 1);
-  for (int i = 1; i < number_of_resources;i++) {
+  coord* resource = new coord[number_of_resources];
+  int type_generator = rand() % 10;
+  for (int i = 0; i < number_of_resources;i++) {
     int found = true;
     while (found == true) {
-      resource[i].x = rand() % (board_size + 1);
-      resource[i].y = rand() % (board_size + 1);
+      resource[i].x = rand() % board_size;
+      resource[i].y = rand() % board_size;
+      type_generator = rand() % 10;
+      if (type_generator == 0)
+        resource[i].type = 'D';
+      else if (type_generator <= 2)
+        resource[i].type = 'G';
+      else if (type_generator <= 5)
+        resource[i].type = 'S';
+      else
+        resource[i].type = 'B';
       found = false;
       for (int j = 0;j <= i - 1;j++)
         if (resource[i].x == resource[j].x && resource[i].y == resource[j].y)
           found = true;
-    }   // resources
-  }                                                      //end
-
+        }
+      }
   Create_Gameboard(board, board_size);
+  cout << number_of_resources << endl;
+  for (int i = 0;i < number_of_resources;i++) {
+    cout << resource[i].x << " " << resource[i].y << " " << resource[i].type << endl;
+    board[resource[i].y + 1][resource[i].x + 1] = resource[i].type;
+  }
+  struct player { //create a player structure
+    int x,y,score;
+  };
+  player p1,p2;
+  int found = true;
+  while (found == true) {
+    found = false;
+    p1.x = rand() % board_size;
+    p1.y = rand() % board_size;
+    p2.x = rand() % board_size;
+    p2.y = rand() % board_size;
+    if (p1.x == p2.x && p1.y == p2.y)
+      found = true;
+    else {
+      for (int i = 0; i < number_of_resources; i++) {
+        if (p1.x == resource[i].x && p1.y == resource[i].y)
+          found = true;
+        if (p2.x == resource[i].x && p2.y == resource[i].y)
+            found = true;
+      }
+    }
+  }
+  board[p1.y + 1][p2.x + 1] = '1';
+  board[p2.y + 1][p2.x + 1] = '2';
   Display_Gameboard(board, board_size);
   system("PAUSE");
   Display_Gamerule();
