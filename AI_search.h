0969c51817b9df0c213b07137e128893981153d7
Ivan-KKH <56263623+Ivan-KKH@users.noreply.github.com>
2020-05-05 22:30:29 +0800

    New

--- AI_search.h
+++ AI_search.h
@@ -1,9 +1,17 @@
+#ifndef AI_SEARCH_H_
+#define AI_SEARCH_H_
+
+#include <ctime>
 #include <stdio.h>
+#include <stdlib>
+
+#endif
 
 #define PLAYER_1 "\xe2\x98\xba"
 #define PLAYER_2 "\xe2\x98\xbb"
 #define DIAMOND_1 "\xe2\x99\xa6"
 #define GOLD "\xE2\x9B\x83"
+
 
 int print_symbol() {
     printf("\x1b[96m\xe2\x99\xa6\n");
@@ -13,10 +21,19 @@ int print_symbol() {
     printf("\x1b[0m");
 }
 
-void easy_mode(){ // to implement the move of computer with easy mode
-
+char easy_mode(){ // to implement the move of computer with easy mode
+  srand(time(NULL));
+  int decision = rand % 4;
+  if (decision == 0)
+    return 'l';
+  else if (decision == 1)
+    return 'r';
+  else if (decision == 2)
+    return 'u';
+  else if (decision == 3)
+    return 'd';
 }
 
-void Hard_mode(){ // to implement the move of computer with hard mode
+char hard_mode(){ // to implement the move of computer with hard mode
 
 }
